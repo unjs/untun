2cb5bffd98aaa24342046384ab7ae5b3764ac058
Anbraten <anton@ju60.de>
2023-10-02 18:24:49 +0200

    Update src/tunnel.ts
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

--- src/tunnel.ts
+++ src/tunnel.ts
@@ -35,7 +35,7 @@ export async function startTunnel(
 
   if (!existsSync(cloudflaredBinPath)) {
     consola.log(cloudflaredNotice);
-    const canInstall = opts.acceptCloudflareNotice || await consola.prompt(
+    const canInstall = (opts.acceptCloudflareNotice || process.env.UNTUN_ACCEPT_CLOUDFLARE_NOTICE) || await consola.prompt(
       `Do you agree with the above terms and wish to install the binary from GitHub?`,
       {
         type: "confirm",
